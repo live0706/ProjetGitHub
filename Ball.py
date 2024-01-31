1a0b30b217d21061397d1ea8a565ec05f906b73d
live0706 <mamadouseck0706@gmail.com>
2024-01-31 18:54:49 +0100

    Reprise ou fin

--- Ball.py
+++ Ball.py
@@ -5,3 +5,7 @@ for i in range(1, 11):
         resultat = nombre * i
         print(f"{nombre} x {i} = {resultat}")
 print("Fin de l'affichage")   
+choix = input("Appuyez sur 'P' pour recommencer, ou une autre touche pour sortir : ").upper()
+if choix != 'P':
+        break
+
