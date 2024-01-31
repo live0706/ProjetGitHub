d55c46c57e78b292d6fc56d126b8b7d26bdc02d8
live0706 <mamadouseck0706@gmail.com>
2024-01-31 18:42:09 +0100

    Ajout de l'affichage des multiples

--- Never.py
+++ Never.py
@@ -2,4 +2,6 @@ while True:
     nombre = int(input("Entrez un nombre : "))
     print(nombre)
 
-    
+for i in range(1, 11):
+        resultat = nombre * i
+        print(f"{nombre} x {i} = {resultat}")   
