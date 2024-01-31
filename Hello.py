987f07cf6f8742c6ba3c79a80831d52d73ae28eb
live0706 <mamadouseck0706@gmail.com>
2024-01-31 14:56:35 +0100

    (f{nombre} x {i} = {resultat})

--- Hello.py
+++ Hello.py
@@ -1,12 +1,13 @@
 while True:
-    nombre = int(input("Entrez un nombre : "))
+    nombre = int(input("Entrer un nombre : "))
     print(nombre)
-
+    
     for i in range(1, 11):
         resultat = nombre * i
         print(f"{nombre} x {i} = {resultat}")
 
-    print('Fin de l\'affichage')
+    print("Fin de l'affichage")
+    
     choix = input("Appuyez sur 'P' pour recommencer, ou une autre touche pour sortir : ").upper()
 
     if choix != 'P':
