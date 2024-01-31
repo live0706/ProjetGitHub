8b9d9d5d2654aecf08e5aec21078a5f71e5d8a53
live0706 <mamadouseck0706@gmail.com>
2024-01-31 12:47:51 +0100

    print(nombre)

--- Hello.py
+++ Hello.py
@@ -1,2 +1,13 @@
-nombre = input("Entrez votre nom")
-print(nombre)
+while True:
+    nombre = int(input("Entrez un nombre : "))
+    print(nombre)
+
+    for i in range(1, 11):
+        resultat = nombre * i
+        print(f"{nombre} x {i} = {resultat}")
+
+    print('Fin de l\'affichage')
+    choix = input("Appuyez sur 'P' pour recommencer, ou une autre touche pour sortir : ").upper()
+
+    if choix != 'P':
+        break
