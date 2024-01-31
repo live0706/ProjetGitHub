20ec0602c4915597a17418d563978411f0c3b7b4
live0706 <mamadouseck0706@gmail.com>
2024-01-31 18:46:57 +0100

    Recommencer ou non

--- Never.py
+++ Never.py
@@ -5,4 +5,9 @@ while True:
 for i in range(1, 11):
         resultat = nombre * i
         print(f"{nombre} x {i} = {resultat}")   
-print("Fin de l'affichage")     
+print("Fin de l'affichage")   
+choix = input("Appuyez sur 'R' pour recommencer, ou une autre touche pour terminer le programme : ").upper()
+choix = input("Appuyez sur 'P' pour recommencer, ou une autre touche pour sortir : ").upper()
+if choix != 'P':
+        break
+
