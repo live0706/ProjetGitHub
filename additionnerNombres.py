ba840ee0c50c7e16616bceea12dcfb8c7ae7e619
live0706 <mamadouseck0706@gmail.com>
2024-03-02 10:19:16 +0100

    additionnerNombres.py

--- additionnerNombres.py
+++
@@ -1,9 +0,0 @@
-def additionnerNombres() :
-    nbre1 = input('entrer un nbre : ')
-    nbre2 = input('entrer un nbre : ')
-    nbre1 = float(nbre1)  
-    nbre2 = float(nbre2)  
-    somme = nbre1 + nbre2
-    print('la somme est ', somme)
-
-additionnerNombres()    
